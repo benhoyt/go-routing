0da3bc3e9fb3c7649851fe068327ebd6a070fbad
Ben Hoyt <benhoyt@gmail.com>
2020-07-23 21:09:21 +1200

    Simpler, faster match()

--- routematch/route.go
+++ routematch/route.go
@@ -24,19 +24,19 @@ func Route(w http.ResponseWriter, r *http.Request) {
 		h = get(apiGetWidgets)
 	case match(p, "/api/widgets"):
 		h = post(apiCreateWidget)
-	case match(p, "/api/widgets/%s", &slug):
+	case match(p, "/api/widgets/+", &slug):
 		h = post(apiWidget{slug}.update)
-	case match(p, "/api/widgets/%s/parts", &slug):
+	case match(p, "/api/widgets/+/parts", &slug):
 		h = post(apiWidget{slug}.createPart)
-	case match(p, "/api/widgets/%s/parts/%d/update", &slug, &id):
+	case match(p, "/api/widgets/+/parts/+/update", &slug, &id):
 		h = post(apiWidgetPart{slug, id}.update)
-	case match(p, "/api/widgets/%s/parts/%d/delete", &slug, &id):
+	case match(p, "/api/widgets/+/parts/+/delete", &slug, &id):
 		h = post(apiWidgetPart{slug, id}.delete)
-	case match(p, "/%s", &slug):
+	case match(p, "/+", &slug):
 		h = get(widget{slug}.widget)
-	case match(p, "/%s/admin", &slug):
+	case match(p, "/+/admin", &slug):
 		h = get(widget{slug}.admin)
-	case match(p, "/%s/image", &slug):
+	case match(p, "/+/image", &slug):
 		h = post(widget{slug}.image)
 	default:
 		http.NotFound(w, r)
@@ -46,36 +46,40 @@ func Route(w http.ResponseWriter, r *http.Request) {
 }
 
 func match(path, pattern string, vars ...interface{}) bool {
-	pathParts := strings.Split(path, "/")
-	patternParts := strings.Split(pattern, "/")
-	if len(pathParts) != len(patternParts) {
-		return false
-	}
-	for i, pathPart := range pathParts {
-		patternPart := patternParts[i]
-		switch patternPart {
-		case "%s":
-			p := vars[0].(*string)
-			vars = vars[1:]
-			if pathPart == "" {
-				return false
+	for ; pattern != "" && path != ""; pattern = pattern[1:] {
+		switch pattern[0] {
+		case '+':
+			// '+' matches till next slash in path
+			slash := strings.IndexByte(path, '/')
+			if slash == 0 {
+				return false // '+' must match at least 1 byte
 			}
-			*p = pathPart
-		case "%d":
-			p := vars[0].(*int)
-			vars = vars[1:]
-			n, err := strconv.Atoi(pathPart)
-			if err != nil || n <= 0 {
-				return false
+			if slash < 0 {
+				slash = len(path)
 			}
-			*p = n
+			segment := path[:slash]
+			path = path[slash:]
+			switch p := vars[0].(type) {
+			case *string:
+				*p = segment
+			case *int:
+				n, err := strconv.Atoi(segment)
+				if err != nil {
+					return false
+				}
+				*p = n
+			default:
+				panic("vars must be *string or *int")
+			}
+			vars = vars[1:]
+		case path[0]:
+			// non-'+' pattern byte must match path byte
+			path = path[1:]
 		default:
-			if pathPart != patternPart {
-				return false
-			}
+			return false
 		}
 	}
-	return true
+	return path == "" && pattern == ""
 }
 
 func allowMethod(h http.HandlerFunc, methods ...string) http.HandlerFunc {
